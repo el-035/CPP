2b72cc581713d12b99765e83e82269c03f4d1946
el-035 <elisa.fittant@gmail.com>
2025-10-13 20:21:08 +0200

    01.02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void)
+{
+	std::string stringSTR = "HI THIS IS BRAIN";
+	std::string* stringPTR = &stringSTR;
+	std::string& stringREF = stringSTR;
+
+	std::cout << "stringSTR address: " << &stringSTR << std::endl;
+	std::cout << "stringPTR address: " << &stringPTR << std::endl;
+	std::cout << "stringREF address: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "stringSTR value: " << stringSTR << std::endl;
+	std::cout << "stringPTR value: " << stringPTR << std::endl;
+	std::cout << "stringREF value: " << stringREF << std::endl;
+
+}
