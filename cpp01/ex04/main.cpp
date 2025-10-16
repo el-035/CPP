680feb98293e6c7ed4da8d61e437ed2090a987d9
Elisa Fittante <efittant@c3r3p3.42vienna.com>
2025-10-16 15:39:12 +0200

    01.04 start

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << "Errror: input three parameters (file, string, string)" << std::endl;
+		return 1;
+	}
+	
+}
