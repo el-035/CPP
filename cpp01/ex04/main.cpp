7f46858907e1e6aedd2dab79b8cf766e8fc89415
el-035 <elisa.fittant@gmail.com>
2025-10-17 16:25:48 +0200

    01.04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,13 +1,65 @@
-#include<iostream>
+#include <fstream>
+#include <iostream>
+#include <string>
 
+bool CheckInput(std::string &filename, std::string &s1, std::string &s2)
+{
+	std::ifstream file(filename);
 
+	if (!file.is_open())
+		return false;
+	if (s1.empty())
+		return false;
+	return true;
+}
+
+void replace(std::string& line, std::string& s1, std::string& s2)
+{
+	std::string* r;
+
+	while(true){
+		r = std::find(line, line.size(), s1);
+
+	}
+}
+
+void ReadWrite(std::string &filename, std::string &s1, std::string &s2)
+{
+	std::string line;
+	while(true){
+		std::ifstream file(filename);
+		/* if (!file.is_open())			has been checked already
+			return ; */
+		std::getline(file, line);	//check eof?
+		if (line.empty())
+			break ;
+		
+	}
+}
 
 int main(int argc, char **argv)
 {
-	if (argc != 4)
-	{
-		std::cout << "Errror: input three parameters (file, string, string)" << std::endl;
-		return 1;
+	if (argc != 4){
+		std::cout << "Input error *_*" << std::endl;
+		return -1;
 	}
+
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+
+	//check input
+	if (!CheckInput(filename, s1, s2)){
+		std::cout << "Input error *_*" << std::endl;
+		return -1;
+	}
+
 	
+	//read file loop
+
+		//readline
+
+		//modify string
+
+		//put to file
 }
