9eda5a7abdcfe46f2ca06d4156244de61508093a
el-035 <elisa.fittant@gmail.com>
2025-10-31 16:08:10 +0100

    03.01 started

---
+++ cpp03/ex01/ClapTrap.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include<iostream>
+
+class ClapTrap{
+	std::string name;
+	unsigned int hitPoints;
+	unsigned int energyPoints;
+	unsigned int attackDamage;
+
+	public:
+	ClapTrap();
+	ClapTrap(const ClapTrap& other);
+	ClapTrap& operator=(const ClapTrap& other);
+	~ClapTrap();
+
+	ClapTrap(std::string name);
+
+	void printPoints(void);
+	
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
