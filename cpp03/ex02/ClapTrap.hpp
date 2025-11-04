a4e797e3963af5fbaf38e64d922630383fd716d4
Elisa Fittante <efittant@c3r6p2.42vienna.com>
2025-11-04 18:18:53 +0100

    03.02 started

---
+++ cpp03/ex02/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include<iostream>
+
+class ClapTrap{
+	protected:
+	std::string name;
+	unsigned int hitPoints;
+	unsigned int energyPoints;
+	unsigned int attackDamage;
+
+	public:
+	ClapTrap();
+	ClapTrap(const ClapTrap& other);
+	ClapTrap& operator=(const ClapTrap& other);
+	virtual ~ClapTrap();
+
+	ClapTrap(std::string name);
+
+	void printPoints(void);
+	
+	virtual void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
---
+++ cpp03/ex02/main.cpp
@@ -0,0 +1,38 @@
+#include "FragTrap.hpp"
+
+int main(void)
+{
+	FragTrap frog("Frog");
+	FragTrap nameless;
+/* 	FragTrap jj = frog;
+
+	jj.highFiveguys();
+	std::cout << std::endl; */
+
+	std::cout << std::endl;
+	nameless.attack("Human");
+	nameless.printPoints();
+
+
+	std::cout << std::endl;
+	frog.attack("Robot");
+	frog.printPoints();
+
+	frog.highFiveguys();
+	std::cout << std::endl;
+
+	frog.beRepaired(5);
+	frog.printPoints();
+	std::cout << std::endl;
+
+	frog.takeDamage(70);
+	frog.printPoints();
+	std::cout << std::endl;
+
+	frog.attack("Human");
+	frog.printPoints();
+	std::cout << std::endl;
+
+	frog.takeDamage(35);
+	frog.printPoints();
+}
