109f2ccf30a07be68e174b839de5e93fb0ae985c
el-035 <elisa.fittant@gmail.com>
2026-01-16 16:26:12 +0100

    work in progress 09.02

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -2,10 +2,14 @@
 
 int comparisons;
 
-int findJack(){ //starts by returing 3, change double prev to 0 if 1 needed
-	static int prev = 1;
-	static int doublePrev = 1;
-	int j = prev + (2 * doublePrev);
+size_t findJack(bool start){ //starts by returing 3, change double prev to 0 if 1 needed
+	static size_t prev = 1;
+	static size_t doublePrev = 0;
+	if (start){
+		prev = 1;
+		doublePrev = 0;
+	}
+	size_t j = prev + (2 * doublePrev);
 	doublePrev = prev;
 	prev = j;
 	return j;
@@ -26,43 +30,120 @@ bool validateDigit(const std::string& input){ 	//duplicates????
 	return true;
 }
 
-void swap(int index, size_t pSize, std::vector<int> &v){
-	std::vector<int> first(v.begin() + index, v.begin() + index + pSize);
-	std::vector<int> second(v.begin() + index + pSize, v.begin() + index + 2 * pSize);
 
-	v.erase(v.begin() + index, v.begin() + index + 2 * pSize);
+void printBlocks(const std::vector<int> &v, size_t bSize)
+{
+	size_t blockSize = bSize * 2;
 
-	v.insert(v.begin() + index, second.begin(), second.end());
-	v.insert(v.begin() + index + pSize, first.begin(), first.end());
-}
+	//std::cout << "BLOCK SIZE: " << bSize << std::endl;
+	for (size_t i = 0; i < v.size(); ++i)
+	{
+		std::cout << v[i];
 
-void sortPairs(size_t &pSize, std::vector<int> &v){
-	size_t index = 0;
-	if (pSize * 2 >= v.size())
-		return ;
-
-	while (index + pSize < v.size()){
-		size_t first = index + pSize -1;
-		size_t second = index + 2 * pSize - 1;
-
-		comparisons++;
-		if (v[first] > v[second])
-			std::swap_ranges(v.begin() + index, v.begin() + index + pSize, v.begin() + index + pSize);
-
-		//	swap(index, pSize, v);
-
-		index += (2 * pSize);
+		if ((i + 1) % blockSize == 0 && i + 1 < v.size())
+			std::cout << " | ";
+		else if ((i + 1) % bSize == 0)
+				std::cout << " - ";
+		else
+			std::cout << " ";
 	}
 
-	/* ------------------------ */
-	std::cout << "AFTER:" << std::endl;
-	for(std::vector<int>::const_iterator it = v.begin(); it != v.end(); ++it)
-		std::cout << (*it) << " ";
 	std::cout << std::endl;
-	/* ------------------------ */
+}
 
-	pSize *=2;
-	sortPairs(pSize, v);	
+void sortPairs(size_t &bSize, std::vector<int> &v){
+	size_t index = 0;
+	if (bSize * 2 >= v.size()){
+		bSize /= 2;
+		return ;
+	}
+	std::cout << "BLOCK SIZE: " << bSize << std::endl;
+	printBlocks(v, bSize);
+	while (index + (bSize * 2) <= v.size()){	//double check this condition
+		size_t first = index + bSize -1;
+		size_t second = index + 2 * bSize - 1;
+
+		comparisons++;	//testing
+		if (v[first] > v[second])
+			std::swap_ranges(v.begin() + index, v.begin() + index + bSize, v.begin() + index + bSize);
+
+		index += (2 * bSize);
+	}
+
+	printBlocks(v, bSize);
+	std::cout << std::endl;
+
+	bSize *=2;
+	sortPairs(bSize, v);	
+}
+
+size_t binSearch(std::vector<int> &v, size_t bSize, size_t maxIndex, int search){
+	size_t minIndex = bSize - 1;
+
+	while (minIndex < maxIndex){
+		size_t midIndex = (minIndex + maxIndex) / 2 * bSize - 1;
+
+		comparisons++;
+		if (search < v[midIndex])
+			maxIndex = midIndex;
+		else
+			minIndex = midIndex + bSize;
+	}
+	
+	return (minIndex);
+}
+
+void mergeInsert(size_t &bSize, std::vector<int> &v){
+	size_t nBlocks = v.size() / bSize;
+	std::cout << "num of blocks: " << nBlocks << std::endl;
+
+	
+	if (nBlocks > 2){
+		findJack(true);
+
+		//move smaller blocks to low vector
+		std::vector<int> low;
+		std::vector<int> main;
+		std::vector<int> left;
+		size_t i = 0;
+		while(i + bSize <= v.size()){
+			size_t block =  i / bSize;
+			if (block < 2)
+				main.insert(main.end(), v.begin() + i, v.begin() + i + bSize);
+			else if (block % 2 == 0)
+				low.insert(low.end(), v.begin() + i, v.begin() + i + bSize);
+			else
+				main.insert(main.end(), v.begin() + i, v.begin() + i + bSize);
+			i += bSize;
+		}
+		if (v.begin() != v.end())
+			left.insert(left.end(), v.begin() + i, v.end());
+
+		v.swap(main);
+		
+		std::cout << "LOW: " << bSize << std::endl;
+		printBlocks(low, bSize);
+		std::cout << "MAIN: " << bSize << std::endl;
+		printBlocks(v, bSize);
+		std::cout << "LEFT: " << bSize << std::endl;
+		printBlocks(left, bSize);
+		
+
+
+		//do insertion of low into v
+		
+
+
+		//add tmp at the end of v
+		v.insert(v.end(), left.begin(), left.end());
+
+
+	}
+	
+	if (bSize == 1)
+		return ;
+	bSize /= 2;
+	//mergeInsert(bSize, v);
 }
 
 
@@ -83,15 +164,22 @@ bool algOne(char **argv, int argc){
 	std::cout << "BEFORE:" << std::endl;
 	for(std::vector<int>::const_iterator it = v.begin(); it != v.end(); ++it)
 		std::cout << (*it) << " ";
-	std::cout << std::endl;
+	std::cout << std::endl << std::endl;
 	/* ------------------------ */
 
 	//sorting pairs
-	size_t pSize = 1;
-	sortPairs(pSize, v);
+	size_t bSize = 1;
+	sortPairs(bSize, v);
 
-	std::cout << pSize << std::endl;
+	//std::cout << bSize << std::endl;
 	//merge vectors
+	bSize /= 2;
+
+	std::cout << "BLOCK SIZE: " << bSize << std::endl;
+	printBlocks(v, bSize);
+	std::cout << std::endl;
+	mergeInsert(bSize, v);
+
 	std::cout << "TOT COMPARISONS: " << comparisons << std::endl;
 	return true;
 }
